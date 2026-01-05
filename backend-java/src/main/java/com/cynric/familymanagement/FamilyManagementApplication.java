95073647ebd82abf2b52678a61dfcab303b0a429
cynric <cynric_ni@163.com>
2026-01-06 00:25:26 +0800

    修改了pom.xml

--- backend-java/src/main/java/com/cynric/familymanagement/FamilyManagementApplication.java
+++ backend-java/src/main/java/com/cynric/familymanagement/FamilyManagementApplication.java
@@ -1,4 +1,4 @@
-package com.family.management;
+package com.cynric.familymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend-java/src/main/java/com/cynric/familymanagement/controller/HealthController.java
+++ backend-java/src/main/java/com/cynric/familymanagement/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.family.management.controller;
+package com.cynric.familymanagement.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
